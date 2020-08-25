12280919c5b3b9babe87ee47f0f4785ccf2bd390
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-08-25 19:22:54 +0200

    fix: remove ':' from protocol

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ module.exports = proxy => {
   if (typeof proxy !== 'string' || !proxy) return undefined
 
   try {
-    const { username, password, hostname, protocol, port } = new URL(proxy)
+    let { username, password, hostname, protocol, port } = new URL(proxy)
     const auth = `${username}:${password}`
-    const toString = () => `${protocol}//${auth}@${hostname}:${port}`
+    protocol = protocol.replace(':', '')
+    const toString = () => `${protocol}://${auth}@${hostname}:${port}`
     return { username, password, hostname, protocol, port, auth, toString }
   } catch (err) {
     throw new ParseProxyError({
--- test/index.js
+++ test/index.js
@@ -10,14 +10,27 @@ test('invalid', t => {
   t.is(parseProxy(''), undefined)
 })
 
-test('valid', t => {
+test('valid (http)', t => {
   const str = 'http://username:password@foo:1337'
   const parsedProxy = parseProxy(str)
 
   t.is(parsedProxy.username, 'username')
   t.is(parsedProxy.password, 'password')
   t.is(parsedProxy.hostname, 'foo')
-  t.is(parsedProxy.protocol, 'http:')
+  t.is(parsedProxy.protocol, 'http')
+  t.is(parsedProxy.port, '1337')
+  t.is(parsedProxy.auth, 'username:password')
+  t.is(parsedProxy.toString(), str)
+})
+
+test('valid (https)', t => {
+  const str = 'https://username:password@foo:1337'
+  const parsedProxy = parseProxy(str)
+
+  t.is(parsedProxy.username, 'username')
+  t.is(parsedProxy.password, 'password')
+  t.is(parsedProxy.hostname, 'foo')
+  t.is(parsedProxy.protocol, 'https')
   t.is(parsedProxy.port, '1337')
   t.is(parsedProxy.auth, 'username:password')
   t.is(parsedProxy.toString(), str)
